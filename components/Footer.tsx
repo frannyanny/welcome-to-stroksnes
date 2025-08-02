5fe21db2b841fc524a0aade9792edf9b1e998c72
Francesca Watson <checker_watson@hotmail.com>
2025-08-02 16:44:09 +0200

    Empty repo

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <div className="text-xs my-2">
-            <p>Copyright Ørjan Laxaa and Francesca Watson (2025)</p>
-         </div> 
-    );
-}
--- components/Intro.tsx
+++
@@ -1,9 +0,0 @@
-export default function Intro() {
-    return (
-        <div className="flex flex-col gap-6">
-            <p className="hidden md:block text-base my-2">Analogue photography by<br/>Ørjan Laxaa and<br/>Francesca Watson</p>
-            <p className="block md:hidden text-base">Analogue photography by Ørjan Laxaa and Francesca Watson</p>
-            <p className="text-xs/5">1st - 30th September 2025 <br/>Cyan Mørkerom, Trondheimsveien, Oslo</p>
-        </div> 
-    );
-}
