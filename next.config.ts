8d3600e81d354e4bdcfa1512da5272a11feb9ce7
Francesca Watson <checker_watson@hotmail.com>
2025-08-03 10:41:46 +0200

    Add base gh pages path to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.PAGES_BASE_PATH,
+ // basePath: process.env.PAGES_BASE_PATH,
+  basePath: "/welcome-to-stroksnes",
   images: { unoptimized: true },
     // Configure `pageExtensions` to include markdown and MDX files
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
